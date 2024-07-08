f9d00794b5ea89de2c614e7bfb8ddbc967ba3c84
leedoyeon849 <do83430208@gmail.com>
2024-07-08 16:47:16 +0900

    feat(Sekeleton): 스켈레톤 뷰 애니메이션 수정

--- Projects/Feature/Common/Sources/UI/SkeletonView.swift
+++ Projects/Feature/Common/Sources/UI/SkeletonView.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 public struct SkeletonView: View {
-    @State private var opacity: Double = 0.5
+    @State private var opacity: Double = 0.1
     
     public init() { }
     
     public var body: some View {
         Rectangle()
-            .fill(Color.gray.opacity(0.1))
+            .fill(Color.black.opacity(0.3))
             .opacity(opacity)
             .onAppear {
                 withAnimation(
                     Animation.linear(duration: 1.0)
                         .repeatForever(autoreverses: true)
-                ) { opacity = 1.0 }
+                ) { opacity = 0.5 }
             }
     }
 }
