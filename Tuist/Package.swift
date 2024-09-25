6d366ed48e624395284498f456732a3099d30778
이도연 <do83430208@gmail.com>
2024-09-25 12:42:30 +0900

    env: swift-dependencies 오류 관련해 Tuist 버전업
    - pakage 접근제어자 관련 대응

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -24,8 +24,7 @@ let packageSettings = PackageSettings(
 let package = Package(
     name: "MediaApp",
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.11.0"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.11.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.0"),
         .package(url: "https://github.com/realm/realm-swift.git", from: "10.50.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.10.0"),
